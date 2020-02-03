f06ef8fd4b632a6ab1bb416c318e87846c7b88a8
CygnusStarion <54466514+CygnusStarion@users.noreply.github.com>
2020-02-03 13:39:16 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,40 @@
+const name = "Joshua";
+let age = 44;
+
+// accessing OBJECT named 'console' and using DOT NOTATION to access a KEY
+// That KEY is a METHOD, so we INVOKE it with ().
+// We also pass in an argument as a STRING.
+// ARGUMENTS provide additional that a METHOD needs to do its job.
+
+console.log("Hello World");
+
+// We are using the '+' as a CONCATENATION OPERATOR.
+// Whenever STRINGS are present, '+'
+
+console.log(
+  "My name is " +
+  name +
+  " and I have been living for " +
+  age +
+" years"
+  )
+
+
+
+
+  // Template Literals and Interpolation
+console.log(`My name is ${name}`);
+
+const timeOfDay = "Afternoon";
+
+console.log(typeof timeOfDay);
+
+console.log(`The variable timeOfDay is of type: ${typeof timeOfDay}.`);
+
+console.log(typeof "hello" + 22);
+
+console.log(22 + 22);
+
+// Dynamic
+let x = 23;
+
